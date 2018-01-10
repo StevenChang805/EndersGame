f650b443f82e9f41cff418ea73ea97df809818f2
zaw min <changs@isyedu.org>
2018-01-10 22:18:43 +0630

    Additions to UFOController

--- Assets/_Completed-Assets/Scripts/UFOController.cs
+++ Assets/_Completed-Assets/Scripts/UFOController.cs
@@ -13,49 +13,39 @@ public class UFOController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		rb2d = GetComponent<Rigidbody2D> ();
+		rb2d = GetComponent<Rigidbody2D>();
 
 	}
 
-	// Update is called once per frame
-	void Update () 
+	void Update()
 	{
-		if (Input.GetKeyDown (KeyCode.W))
+		if (Input.GetKeyDown(KeyCode.W))
 		{
-			moveUP();
-		}
-		if (Input.GetKeyDown (KeyCode.A))
-		{
-			moveLEFT();
-		}
+			movevertical();
+		}	
 		if (Input.GetKeyDown (KeyCode.S))
 		{
-			moveDOWN();
+			movevertical();
 		}
-		if (Input.GetKeyDown (KeyCode.D))
+		else if (Input.GetKeyDown (KeyCode.A))
 		{
-			moveRIGHT();
+			movehorizontal();
+		}
+		else if (Input.GetKeyDown (KeyCode.D))
+		{
+			movehorizontal();
 		}
 	}
 
-	void moveUP()
+	void movevertical()
 	{
 		float vertical = Input.GetAxis ("Vertical");
-		//0 makes sure it does not move along x-axis
-		Vector2 moveVertical = new Vector2 (0, vertical);
+			Vector2 moveVertical = new Vector2 (0, vertical);
 
 		rb2d.AddForce (moveVertical * speed);
-	}
+	} 
 
-	void moveDOWN()
-	{
-		float vertical = Input.GetAxis ("Vertical");
-		Vector2 moveVertical = new Vector2 (0, vertical);
-
-		rb2d.AddForce (moveVertical * speed);
-	}
-
-	void moveRIGHT()
+	void movehorizontal()
 	{
 		float horizontal = Input.GetAxis ("Horizontal");
 		Vector2 moveHorizontal = new Vector2 (horizontal, 0);
@@ -63,15 +53,12 @@ public class UFOController : MonoBehaviour {
 		rb2d.AddForce (moveHorizontal * speed);
 	}
 
-	void moveLEFT()
+	void Decrement() 
 	{
-		float horizontal = Input.GetAxis ("Horizontal");
-		Vector2 moveHorizontal = new Vector2 (horizontal, 0);
-
-		rb2d.AddForce (moveHorizontal * speed);
-	}
-
-	void Decrement() {
 		health -= 20;
+		
+		if (health == 0)
+			Destroy(gameObject);;
 	}
+
 }
